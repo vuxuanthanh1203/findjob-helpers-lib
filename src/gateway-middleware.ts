61a668459ded1599e57b75e77d544bdf1822b4da
xthanh12 <vuxuanthanh1203@gmail.com>
2024-10-17 17:18:26 +0700

    update gateway middleware

--- src/gateway-middleware.ts
+++ src/gateway-middleware.ts
@@ -14,7 +14,7 @@ export function verifyGatewayRequest(req: Request, res: Response, next: NextFunc
   }
 
   try {
-    const payload: { id: string; iat: number } = JWT.verify(token, '1282722b942e08c8a6cb033aa6ce850e') as { id: string; iat: number };
+    const payload: { id: string; iat: number } = JWT.verify(token, '14535b4b322865943ac2ee2f4e8fca6b') as { id: string; iat: number };
     if (!tokens.includes(payload.id)) {
       throw new NotAuthorizedError('Invalid request', 'Request payload is invalid');
     }
